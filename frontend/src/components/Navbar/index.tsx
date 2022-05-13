b0e4f41fc8ec7659ac594c731e9f00fb1aee4597
Alessandre S Cabral <alesssandresouzacabral@gmail.com>
2022-05-13 18:22:56 -0300

    Mudar Nome do Nav Bar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -12,7 +12,7 @@ function Navbar() {
           <a href="https://github.com/devsuperior/">
             <div className='dsmovie-contact-container'>
               <GithubIcon />
-              <p className='dsmovie-contact-link'>/devsuperior</p>
+              <p className='dsmovie-contact-link'>/alessandrecabral</p>
             </div>
           </a>
         </div>
